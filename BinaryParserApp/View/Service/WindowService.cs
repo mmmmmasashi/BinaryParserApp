f12396be2352e6afd0a0723dedcab92e948a7534
m_masashi <drift_tms@yahoo.co.jp>
2025-06-21 23:58:31 +0900

    update : show tsv window, instead of showing it in MainWindow.

---
+++ BinaryParserApp/View/Service/WindowService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BinaryParserApp.View.Service
+{
+    public class WindowService : IWindowService
+    {
+        public void ShowTextWindow(string text)
+        {
+            var window = new View.TextWindow(text);
+            window.Owner = System.Windows.Application.Current.Windows
+                        .OfType<Window>().FirstOrDefault(w => w.IsActive);
+            window.ShowDialog();
+        }
+    }
+}
---
+++ BinaryParserApp/ViewModel/TextWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+using BinaryParserApp.View.Service;
+using Reactive.Bindings;
+
+namespace BinaryParserApp.ViewModel
+{
+    public class TextWindowViewModel
+    {
+        public ReactiveProperty<string> DisplayText { get; set; }
+        public ICommand CloseCommand { get; }
+
+        public event Action? RequestClose;
+
+        public TextWindowViewModel(string text)
+        {
+            DisplayText = new ReactiveProperty<string>(text);
+            CloseCommand = new ReactiveCommand()
+                .WithSubscribe(() => RequestClose?.Invoke());
+        }
+    }
+}
